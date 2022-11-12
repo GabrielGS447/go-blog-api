8e15d3296b65ef18e6c09024200849c584ee84af
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-12 17:23:08 -0300

    feature: remove 'unique' constraint from user display_name

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ import (
 
 type User struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
-	DisplayName string    `json:"display_name" gorm:"unique; not null"`
+	DisplayName string    `json:"display_name" gorm:"not null"`
 	Email       string    `json:"email" gorm:"unique; not null"`
 	Password    string    `json:"password" gorm:"not null"`
 	CreatedAt   time.Time `json:"created_at"`
