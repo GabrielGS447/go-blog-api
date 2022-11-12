aa464decd4e56103b1e23fdac747e15d20d16779
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-12 17:02:51 -0300

    feature: create input validation response helper

---
+++ utils/input-validation-response.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"errors"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func GetErrorResponse(err error) []string {
+	var ve validator.ValidationErrors
+	if errors.As(err, &ve) {
+		out := make([]string, len(ve))
+		for i, e := range ve {
+			out[i] = getErrorMsg(e)
+		}
+		return out
+	}
+	return nil
+}
+
+func getErrorMsg(fe validator.FieldError) string {
+	switch fe.Tag() {
+	case "required":
+		return fe.Field() + " is required"
+	case "email":
+		return fe.Field() + " must be a valid email"
+	case "min":
+		return fe.Field() + " must be at least " + fe.Param() + " characters"
+	case "max":
+		return fe.Field() + " must be at most " + fe.Param() + " characters"
+	default:
+		return "An unknown error occurred"
+	}
+}
