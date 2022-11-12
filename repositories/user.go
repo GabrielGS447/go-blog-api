4a5b5bde2e2c5616f59bd0a161c0f298c079ddf9
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-12 19:33:15 -0300

    refactor: change UserFindByEmail to avoid ErrRecordNotFound

--- repositories/user.go
+++ repositories/user.go
@@ -1,17 +1,13 @@
 package repositories
 
 import (
-	"github.com/gabrielgaspar447/go-blog-api/constants"
 	"github.com/gabrielgaspar447/go-blog-api/db"
 	"github.com/gabrielgaspar447/go-blog-api/models"
 )
 
 func UserFindByEmail(email string) (models.User, error) {
 	var user models.User
-	err := db.DB.Where("email = ?", email).First(&user).Error
-	if err != nil && err.Error() == constants.GormNotFound {
-		return user, nil
-	}
+	err := db.DB.Limit(1).Find(&user, "email = ?", email).Error
 	return user, err
 }
 
