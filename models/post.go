67734225ae334e17c61ad2cbad073146371f2eec
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-11-12 23:17:20 -0300

    refactor: change time fields to pointer types; add omitempty

--- models/post.go
+++ models/post.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type Post struct {
-	ID        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title" gorm:"not null" binding:"required,min=3,max=100"`
-	Content   string    `json:"content" gorm:"not null" binding:"required,min=3"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	UserID    uint      `json:"user_id,omitempty" gorm:"not null"`
+	ID        uint       `json:"id,omitempty" gorm:"primaryKey"`
+	Title     string     `json:"title" gorm:"not null" binding:"required,min=3,max=100"`
+	Content   string     `json:"content" gorm:"not null" binding:"required,min=3"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	UserID    uint       `json:"user_id,omitempty" gorm:"not null"`
+	User      *User      `json:"user,omitempty" gorm:"foreignKey:UserID"`
 }
--- models/user.go
+++ models/user.go
@@ -5,13 +5,13 @@ import (
 )
 
 type User struct {
-	ID          uint      `json:"id" gorm:"primaryKey"`
-	DisplayName string    `json:"display_name" gorm:"not null" binding:"required,min=3,max=20"`
-	Email       string    `json:"email" gorm:"unique; not null" binding:"required,email"`
-	Password    string    `json:"password,omitempty" gorm:"not null" binding:"required,min=6"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	Posts       []Post    `json:"posts,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	ID          uint       `json:"id,omitempty" gorm:"primaryKey"`
+	DisplayName string     `json:"display_name" gorm:"not null" binding:"required,min=3,max=20"`
+	Email       string     `json:"email" gorm:"unique; not null" binding:"required,email"`
+	Password    string     `json:"password,omitempty" gorm:"not null" binding:"required,min=6"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+	Posts       []Post     `json:"posts,omitempty" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
 type LoginDTO struct {
