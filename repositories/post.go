35e10740d7188cfd189da00b24adbc9ba3537b82
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2022-12-03 13:24:38 -0300

    Remove unnecessary 'pointer to pointer' in repositories

--- repositories/post.go
+++ repositories/post.go
@@ -11,26 +11,26 @@ func PostCreate(input *models.Post) error {
 
 func PostList(posts *[]models.Post, includeUser bool) error {
 	if includeUser {
-		return db.DB.Preload("User").Find(&posts).Error
+		return db.DB.Preload("User").Find(posts).Error
 	}
 
-	return db.DB.Find(&posts).Error
+	return db.DB.Find(posts).Error
 }
 
 func PostGetById(post *models.Post, id uint, includeUser bool) error {
 	if includeUser {
-		return db.DB.Preload("User").Find(&post, id).Error
+		return db.DB.Preload("User").Find(post, id).Error
 	}
 
-	return db.DB.Find(&post, id).Error
+	return db.DB.Find(post, id).Error
 }
 
 func PostSearch(posts *[]models.Post, query string, includeUser bool) error {
 	if includeUser {
-		return db.DB.Preload("User").Where("title LIKE ?", "%"+query+"%").Find(&posts).Error
+		return db.DB.Preload("User").Where("title LIKE ?", "%"+query+"%").Find(posts).Error
 	}
 
-	return db.DB.Where("title LIKE ?", "%"+query+"%").Find(&posts).Error
+	return db.DB.Where("title LIKE ?", "%"+query+"%").Find(posts).Error
 }
 
 func PostUpdate(input *models.Post, id uint) error {
--- repositories/user.go
+++ repositories/user.go
@@ -18,18 +18,18 @@ func UserCreate(input *models.User) error {
 func UserList(users *[]models.User, includePosts bool) error {
 
 	if includePosts {
-		return db.DB.Omit("Password").Preload("Posts").Find(&users).Error
+		return db.DB.Omit("Password").Preload("Posts").Find(users).Error
 	}
 
-	return db.DB.Omit("Password").Find(&users).Error
+	return db.DB.Omit("Password").Find(users).Error
 }
 
 func UserFindById(user *models.User, id uint, includePosts bool) error {
 	if includePosts {
-		return db.DB.Omit("Password").Preload("Posts").Find(&user, id).Error
+		return db.DB.Omit("Password").Preload("Posts").Find(user, id).Error
 	}
 
-	return db.DB.Omit("Password").Find(&user, id).Error
+	return db.DB.Omit("Password").Find(user, id).Error
 }
 
 func UserDeleteById(id uint) error {
